f597fda67adbdb294167e011eba518f2ee8bfb84
figo5530 <figooooo0221@gmail.com>
2021-03-01 17:36:04 -0500

    enbales session, defines and implements helpers method #logged_in? and #current_user in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,21 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enbale :sessions
+    set :session_secret, "Recipe"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 end
