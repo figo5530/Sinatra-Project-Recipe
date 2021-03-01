3709b0cb0335b4a63b48e436c5e947fa41b0fda0
figo5530 <figooooo0221@gmail.com>
2021-03-01 16:56:21 -0500

    removes whatever corneal wrong configuration and recreates new configurations and migrate users table

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+    has_secure_password
+    validates :username, presence: true
+    validates :email, presence: true
+    validates_uniqueness_of :username
+    validates_uniqueness_of :email
+end
