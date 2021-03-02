fc784692c1af65bae38218cdae16263158386000
figo5530 <figooooo0221@gmail.com>
2021-03-02 14:48:39 -0500

    creates and finishes post /recipes/new and get /recipes/:id routes in RecipesController and /recipes/show.erb with layout_1.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,4 +14,23 @@ class RecipesController < ApplicationController
             redirect '/login'
         end
     end
+
+    post '/recipes/new' do
+        if logged_in?
+            @recipe = current_user.authored_recipes.build(name: params[:name], ingredients: params[:ingredients], cooking_time: params[:cooking_time], level: params[:level], steps: params[:steps], save_times: 0)
+            @recipe.save
+            redirect "/recipes/#{@recipe.id}"
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/recipes/:id' do
+        if logged_in?
+            @recipe = Recipe.find_by(id: params[:id])
+            erb :"recipes/show", :layout => :layout_1
+        else
+            redirect '/login'
+        end
+    end
 end
