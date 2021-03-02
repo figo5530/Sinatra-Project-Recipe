8aca391895b3be9b3a09cbb2ee09e6cae3393cc2
figo5530 <figooooo0221@gmail.com>
2021-03-02 18:04:42 -0500

    creates and finishes get /recipes/:id/edit in RecipesController, finishes recipes/edit.erb with layout_3

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
         end
     end
 
-    post '/recipes/new' do
+    post '/recipes' do
         if logged_in?
             @recipe = current_user.authored_recipes.build(name: params[:name], ingredients: params[:ingredients], cooking_time: params[:cooking_time], level: params[:level], steps: params[:steps], save_times: 0)
             @recipe.save
@@ -33,4 +33,17 @@ class RecipesController < ApplicationController
             redirect '/login'
         end
     end
+
+    #get edit
+    get '/recipes/:id/edit' do
+        if logged_in?
+            @recipe = Recipe.find_by(id: params[:id])
+            erb :"recipes/edit", :layout => :layout_3
+        else
+            redirect '/login'
+        end
+    end
+    #patch 
+    #delete
+    #save
 end
